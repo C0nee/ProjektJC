b6d912b496d633a7bdd4ee480e18a2bf55220476
C0nee <jakubczajkowski04@wp.pl>
2023-02-08 14:55:02 +0100

    Sprawdzanie czy plik jest obrazkiem

--- folder/index.php
+++ folder/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MEM</title>
 </head>
 <body>
     <form action="" method="post" enctype="multipart/form-data">
@@ -23,6 +23,20 @@ $targetDir = "img/";
 $sourceFileName =  $_FILES['uploadedFile']['name'];
 $tempURL = $_FILES['uploadedFile']['tmp_name'];
 $targetURL = $targetDir . $sourceFileName;
+
+if(file_exists($targetURL)){
+    die("JEST PLIK");
+}
+//sprawdz czy mamy doczynienia z obrazem
+$imginfo = getimagesize($tempURL);
+if(!is_array($imginfo)){
+    die("nie jest obrazek");
+}
+
+
+
+
+
 move_uploaded_file($tempURL , $targetURL);
 }
 
