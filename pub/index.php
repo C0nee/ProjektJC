ca4a872292742119d181bc4418c23853400e56df
C0nee <jakubczajkowski04@wp.pl>
2023-03-22 15:54:02 +0100

    ASdA

--- pub/index.php
+++ pub/index.php
@@ -1,30 +1,73 @@
 <?php
 require_once('./../src/config.php');
+//musi być wywołane zanim jakikolwiek content trafi do przeglądarki użytkownika
+session_start();
 
 use Steampixel\Route;
 
-Route::add('/', function(){
-    //zapisywanie obrazków
+Route::add('/', function() {
+    //strona wyświetlająca obrazki
     global $twig;
+    //pobierz 10 najnowszych postów
     $postArray = Post::getPage();
     $twigData = array("postArray" => $postArray,
-                        "pageTitle" =>"strona główna");
-    $twig->display("index.html.twig",$twigData);
+                        "pageTitle" => "Strona główna",
+                        );
+    //jeśli użytkownik jest zalogowany to przekaż go do twiga
+    if(isset($_SESSION['user']))
+        $twigData['user'] = $_SESSION['user'];
+    $twig->display("index.html.twig", $twigData);
 });
-Route::add('/upload', function(){
-    //strona do wgrywania
+
+Route::add('/upload', function() {
+    //strona z formularzem do wgrywania obrazków
     global $twig;
     $twigData = array("pageTitle" => "Wgraj mema");
-    $twig->display("upload.html.twig",$twigData);
+    //jeśli użytkownik jest zalogowany to przekaż go do twiga
+    if(isset($_SESSION['user']))
+        $twigData['user'] = $_SESSION['user'];
+    $twig->display("upload.html.twig", $twigData);
 });
-Route::add('/upload',function(){
+
+Route::add('/upload', function() {
+    //wywoła się tylko po otrzymaniu danych metodą post na ten url
+    // (po wypełnieniu formularza)
     global $twig;
     if(isset($_POST['submit']))  {
-        Post::upload($_FILES['uploadedFile']['tmp_name']);
-    };
+        Post::upload($_FILES['uploadedFile']['tmp_name'], $_POST['title'], $_POST['userId']);
+    }
     //TODO: zmienić na ścieżkę względną
     header("Location: http://localhost/ProjektJC/pub");
-},'post');
+}, 'post');
 
-Route::run('/ProjektJC/pub')
+Route::add('/register', function() {
+    global $twig;
+    $twigData = array("pageTitle" => "Zarejestruj użytkownika");
+    $twig->display("register.html.twig", $twigData);
+});
+
+Route::add('/register', function(){
+    global $twig;
+    if(isset($_POST['submit'])) {
+        User::register($_POST['email'], $_POST['password']);
+        header("Location: http://localhost/ProjektJC/pub");
+    }
+}, 'post');
+
+Route::add('/login', function(){
+    global $twig;
+    $twigData = array("pageTitle" => "Zaloguj użytkownika");
+    $twig->display("login.html.twig", $twigData);
+});
+
+Route::add('/login', function() {
+    global $twig;
+    if(isset($_POST['submit'])) {
+        User::login($_POST['email'], $_POST['password']);
+    }
+    header("Location: http://localhost/ProjektJC/pub");
+
+}, 'post');
+
+Route::run('/ProjektJC/pub');
 ?>
